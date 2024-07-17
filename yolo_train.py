82b61de17b3ef585e83e0206d2f9c495393f6963
reda rhanmouni <65564781+mreda1998@users.noreply.github.com>
2024-07-17 07:05:48 -0400

    wrong path

--- yolo_train.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-
-if __name__ == '__main__':
-    # Load a model
-    model = YOLO("./yolov8n.pt")
-
-    # Train the model
-    model.train(data="data.yaml", epochs=3, imgsz=640, device='mps')
