fbd0231add6a704d86bd52620d405fc9c6ab4ae9
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2024-09-30 08:34:15 -0400

    feat: normalize labels

--- src/yolo/yolo_train.py
+++ src/yolo/yolo_train.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO
-
+from normalize_labels import normalize_labels
 
 if __name__ == '__main__':
     # Load a model
     model = YOLO("./yolov8n.pt")
 
+    # Normalize the labels
+    normalize_labels()
+
     # Train the model
-    model.train(data="data.yaml", epochs=3, imgsz=640, device='mps')
+    model.train(data="../data.yaml", epochs=50, imgsz=640, device='mps')
