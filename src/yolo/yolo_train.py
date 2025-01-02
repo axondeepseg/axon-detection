ffc31a9fe1fb86dee1d933882f66b73dbc540abd
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2025-01-02 08:11:49 -0500

    chore: cleaned train file

--- src/yolo/yolo_train.py
+++ src/yolo/yolo_train.py
@@ -1,5 +1,3 @@
-import wandb
-import os
 from ..constants.wandb_yolo_constants import WANDB_ENTITY, WANDB_PROJECT, WANDB_RUN_NAME, WANDB_RUN_ID
 from ..yolo.wandb_trainer import WandbTrainer
   
@@ -9,8 +7,10 @@ if __name__ == "__main__":
     config = {
         # NOTE: "datasets_dir" in settings.json of Ultralytics should look like this:  "\\axon-detection"
         'data': 'src/data-yolo/data.yaml',
-        'epochs': 138,
+        'epochs': 250,
         'imgsz': 640,
+        'optimizer': 'adam',
+        'rect': True,
         'batch': 16,
         'project': WANDB_PROJECT,
         'name': WANDB_RUN_NAME
@@ -18,5 +18,7 @@ if __name__ == "__main__":
 
     trainer = WandbTrainer(model_path="./yolov8n.pt", config=config)    
     trainer.run_step()
+    # example usage to visualize ground truth
+    # trainer.visualize_ground_truth(test_dir="src/data-yolo/images/val", labels_dir="src/data-yolo/labels/val")
     
 
