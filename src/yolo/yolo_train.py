1932e4d14aa699ed47f5cc892d0ad5ccd3fa287e
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2025-01-09 15:54:55 -0500

    chore: cleaned paths

--- src/yolo/yolo_train.py
+++ src/yolo/yolo_train.py
@@ -4,6 +4,11 @@ import os
 SRC_PATH = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(SRC_PATH)
 
+DATA_PATH = os.path.abspath(os.path.join(SRC_PATH, 'data-yolo'))
+DATA_YAML_PATH = os.path.join(DATA_PATH, 'data.yaml')
+IMAGES_PATH = os.path.join(DATA_PATH, 'images', 'test')
+LABELS_PATH = os.path.join(DATA_PATH, 'labels', 'test')
+
 from constants.wandb_yolo_constants import WANDB_PROJECT, WANDB_RUN_NAME
 from yolo.wandb_trainer import WandbTrainer
   
@@ -12,7 +17,7 @@ if __name__ == "__main__":
 
     config = {
         # NOTE: "datasets_dir" in settings.json of Ultralytics should look like this:  "\\axon-detection"
-        'data': os.path.abspath(os.path.join(SRC_PATH, 'data-yolo', 'data.yaml')),
+        'data': DATA_YAML_PATH,
         'epochs': 250,
         'imgsz': 640,
         'optimizer': 'adam',
@@ -24,7 +29,8 @@ if __name__ == "__main__":
 
     trainer = WandbTrainer(model_path="./yolov8n.pt", config=config)    
     trainer.run_step()
-    # example usage to visualize ground truth
-    # trainer.visualize_ground_truth(test_dir="src/data-yolo/images/val", labels_dir="src/data-yolo/labels/val")
+    
+    # example usage to visualize ground truth - change lines 9 and 10 if you want to see the ground truths for another set
+    # trainer.visualize_ground_truth(test_dir=IMAGES_PATH, labels_dir=LABELS_PATH)
     
 
