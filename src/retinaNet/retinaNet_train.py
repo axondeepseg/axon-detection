0dc59e8d189d9c921507b48b2d87956c65432e20
Muriellehxh <90001299+Muriellehxh@users.noreply.github.com>
2025-01-15 23:23:19 -0500

    smaller iteration number for quick test

--- src/retinaNet/retinaNet_train.py
+++ src/retinaNet/retinaNet_train.py
@@ -108,7 +108,7 @@ def configure_detectron():
 
     cfg.SOLVER.IMS_PER_BATCH = 1
     cfg.SOLVER.BASE_LR = 0.001
-    cfg.SOLVER.MAX_ITER = 240  # (2*140)/8 = 60 epochs
+    cfg.SOLVER.MAX_ITER = 20  # (2*140)/8 = 60 epochs
     cfg.SOLVER.STEPS = [40, 60]  # no learning decay (lr remains stable)
     # cfg.SOLVER.GAMMA = 0.1  # decay factor for lr
     cfg.SOLVER.LR_SCHEDULER_NAME = "WarmupCosineLR"  # scheduler for early warmup
